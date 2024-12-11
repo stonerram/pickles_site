989cc92d02e24f8e589a7c4a6bc7c697f18c39bc
stonerram <stonerram488@gmail.com>
2024-12-11 15:55:05 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import AdCarousel from './components/AdCarousel';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <AdCarousel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/AdCarousel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdCarousel() {
+  return (
+    <div>
+      <h1>Carousel</h1>
+    </div>
+  )
+}
+
+export default AdCarousel
